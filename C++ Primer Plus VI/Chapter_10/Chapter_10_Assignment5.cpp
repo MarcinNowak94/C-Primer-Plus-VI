2c5784c1388b112853ef156cc7b08bf8bcbdc0e8
Marcin Nowak <marcin94nowak@gmail.com>
2017-09-24 23:36:22 +0200

    Done Chapter_10_Assignment5

--- C++ Primer Plus VI/Chapter_10/Chapter_10_Assignment5.cpp
+++ C++ Primer Plus VI/Chapter_10/Chapter_10_Assignment5.cpp
@@ -1,5 +1,6 @@
 #include "../stdafx.h"
 #include "Chapter_10_Functions.h"
+#include "stack.h"
 
 /*
 5. Consider the following structure declaration:
@@ -17,8 +18,27 @@ the typedef declaration so that Item is type customer instead of unsigned long.
 
 int Chapter_10_Assignment5()
 {
-	std::cout << "\a\nNothing to see here (YET), move along.";
-	std::cout << "\nPress any key to contine..." << std::endl;
+	//std::cout << "\a\nNothing to see here (YET), move along.";
+	//std::cout << "\nPress any key to contine..." << std::endl;
+	long double runningtotal = 0;
+	Stack stack;
+	customer customertab[]=
+	{
+		{"Customer 1", 350},
+		{"Customer 2", 450},
+		{"Customer 3", 10},
+		{"George", 2.56}
+	};
+	for (size_t i = 0; i < (sizeof(customertab)/sizeof(customertab[0])); i++)
+	{
+		stack.push(customertab[i]);
+	};
+	for (size_t i = (sizeof(customertab) / sizeof(customertab[0]))-1; !stack.isempty(); i--)
+	{
+		runningtotal += customertab[i].payment;
+		stack.pop(customertab[i]);
+		std::cout << "\nRunning total =" << runningtotal;
+	};
 	_getch();
 	return 0;
 };
---
+++ C++ Primer Plus VI/Chapter_10/Stack.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef STCAK_H_
+#define STACK_H_
+
+struct customer
+{
+	char fullname[30];
+	double payment;
+};
+
+typedef customer Item;
+
+class Stack
+{
+private:
+	enum { MAX = 10 };        //constant specific to class
+	Item items[MAX];          //holds stack items
+	int top;                  //index for top stack itemp
+public:
+	Stack();
+	bool isempty() const;
+	bool isfull() const;
+	//push() returns false if stack already is full, true otherwise
+	bool push(const Item & item);                 //add item to stack
+	//pop() returns false if stack already is empty, true otherwise
+	bool pop(Item & item);                        //pop top into item
+};
+#endif
