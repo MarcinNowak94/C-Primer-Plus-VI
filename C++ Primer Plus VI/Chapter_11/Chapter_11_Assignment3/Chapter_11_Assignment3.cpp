2c5a99de4476c023212b698048fa83ccdb7c8d0f
Marcin Nowak <marcin94nowak@gmail.com>
2017-10-01 11:43:48 +0200

    Done Chapter_11_Assignment3

--- C++ Primer Plus VI/Chapter_11/Chapter_11_Assignment3/Chapter_11_Assignment3.cpp
+++ C++ Primer Plus VI/Chapter_11/Chapter_11_Assignment3/Chapter_11_Assignment3.cpp
@@ -1,11 +1,51 @@
 #include "../../stdafx.h"
 #include "../Chapter_11_Functions.h"
 #include "vect_3.h"
+// randwalk.cpp -- test klasy Vector
+// kompilowaæ z plikiem vect.cpp
+#include <iostream>
+#include <cstdlib>    // prototypy funkcji rand() i srand()
+#include <ctime>      // prototyp funkcji time()
+
 
 int Chapter_11_Assignment3()
 {
-	std::cout << "\n\aNothing to see here (YET). Move along.";
-	std::cout << "\nPress any key to continue ...";
+	using namespace std;
+	using VECTOR_3::Vector;
+	srand(time(0));   // inicjalizacja generatora liczb pseudolosowych
+	double direction;
+	Vector step;
+	Vector result(0.0, 0.0);
+	unsigned long steps = 0;
+	double target = random(100,200);
+	double dstep = random(double(1), target - 1);
+	int trials = 0;
+	double min = DBL_MAX;
+	double max = DBL_MIN;
+	double avg = 0;
+	std::cout << "Input amount of trials: ";
+	std::cin >> trials;
+	int i = 0;
+	while (i++ < trials)
+	{
+		while (result.magval() < target)
+		{
+			direction = rand() % 360;
+			step.reset(dstep, direction, Vector::POL);
+			result = result + step;
+			steps++;
+		}
+		std::cout << "--Trial#" << i <<"\t("<< steps << "steps)\n";
+		if (steps < min) min = steps;
+		if (steps > max) max = steps;
+		avg += steps;
+		steps = 0;
+		result.reset(0.0, 0.0);
+	};
+	std::cout << "\nDistance to travel: " << target << ", step size: " << dstep
+		<< "\nAverage of " << trials << " trials is: " << avg / trials
+		<< "\nMin amount of steps: " << min
+		<< "\nMax amount of steps: " << max;
 	_getch();
 	return 0;
 };
@@ -17,52 +57,4 @@ particular target/step combination,it reports the highest,lowest,and average num
 of steps for N trials,where N is an integer entered by the user.
 */
 
-// randwalk.cpp -- test klasy Vector
-// kompilowaæ z plikiem vect.cpp
-#include <iostream>
-#include <cstdlib>    // prototypy funkcji rand() i srand()
-#include <ctime>      // prototyp funkcji time()
-#include "vect_3.h"
 
-int Vect_3()
-{
-	using namespace std;
-	using VECTOR_3::Vector;
-	srand(time(0));   // inicjalizacja generatora liczb pseudolosowych
-	double direction;
-	Vector step;
-	Vector result(0.0, 0.0);
-	unsigned long steps = 0;
-	double target;
-	double dstep;
-	cout << "Podaj dystans do przejœcia (k, aby zakoñczyæ): ";
-	while (cin >> target)
-	{
-		cout << "Podaj d³ugoœæ kroku: ";
-		if (!(cin >> dstep))
-			break;
-
-		while (result.magval() < target)
-		{
-			direction = rand() % 360;
-			step.reset(dstep, direction, Vector::POL);
-			result = result + step;
-			steps++;
-		}
-		cout << "Po " << steps << " krokach delikwent "
-			"osi¹gn¹³ po³o¿enie:\n";
-		cout << result << endl;
-		result.polar_mode();
-		cout << " czyli\n" << result << endl;
-		cout << "Œrednia d³ugoœæ kroku pozornego = "
-			<< result.magval() / steps << endl;
-		steps = 0;
-		result.reset(0.0, 0.0);
-		cout << "Podaj dystans do przejœcia (k, aby zakoñczyæ): ";
-	}
-	cout << "Koniec!\n";
-	cin.clear();
-	while (cin.get() != '\n')
-		continue;
-	return 0;
-}
