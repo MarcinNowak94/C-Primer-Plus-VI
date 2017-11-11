bafc759c0a9c34796862066defbd6f16495651e3
Marcin Nowak <marcin94nowak@gmail.com>
2017-11-11 12:50:45 +0100

    Done Chapter_12_Assignment5

--- C++ Primer Plus VI/Chapter_12/Chapter_12_Assignment5/Chapter_12_Assignment5.cpp
+++ C++ Primer Plus VI/Chapter_12/Chapter_12_Assignment5/Chapter_12_Assignment5.cpp
@@ -11,7 +11,7 @@
 */
 const int MIN_PER_HR = 60;
 const int MAX_TIME_TO_WAIT = 1;
-const int TRIAL_PERIOD = 100 * MIN_PER_HR;
+const int TRIAL_PERIOD = 100;
 
 bool newcustomer(double x);    // czy dotar³ ju¿ nastêpny klient?
 
@@ -25,23 +25,10 @@ int Chapter_12_Assignment5()
 	std::srand(std::time(0));  // inicjalizacja generatora liczb losowych
 
 	cout << "Studium przypadku: bankomat Banku Stu Kas\n";
-	cout << "Podaj maksymaln¹ d³ugoœæ kolejki: ";
-	int qs;
-	cin >> qs;
-	Queue line(qs);            // w kolejce mo¿e stan¹æ do qs klientów
-
-	cout << "Podaj symulowany czas (w godzinach): ";
-	int hours;                 // liczba godzin symulacji
-	cin >> hours;
-	// symulacja z jednominutow¹ rozdzielczoœci¹
-	long cyclelimit = MIN_PER_HR * hours;       // liczba cykli
-
-	cout << "Podaj œredni¹ liczbê klientów na godzinê: ";
-	double perhour;            // œrednia liczba nowych klientów na godzinê
-	cin >> perhour;
-	double min_per_cust;       // œredni odstêp czasowy pomiêdzy klientami
-	min_per_cust = MIN_PER_HR / perhour;
-
+	
+	int qs = 20;
+	double perhour=random(20,25);
+	
 	Item temp;                 // dane nowego klienta
 	long turnaways = 0;        // liczba klientów odes³anych z kolejki
 	long customers = 0;        // liczba klientów przyjêtych do kolejki
@@ -50,51 +37,65 @@ int Chapter_12_Assignment5()
 	int wait_time = 0;         // czas do zwolnienia bankomatu
 	long line_wait = 0;        // ³¹czny czas oczekiwania
 
-							   // rozpoczêcie symulacji
-	for (int cycle = 0; cycle < cyclelimit; cycle++)
+
+
+
+	do
 	{
-		if (newcustomer(min_per_cust))  // mamy nowego chêtnego
+		cout << "\n\nMaximum queue length: " << qs << '\n';
+		Queue line(qs);            // w kolejce mo¿e stan¹æ do qs klientów
+		cout << "Simulated time: " << TRIAL_PERIOD << '\n';
+		long cyclelimit = MIN_PER_HR * TRIAL_PERIOD;       // liczba cykli
+		cout << "Average amount of clients per hour: " << perhour-- << '\n';
+		double min_per_cust;       // œredni odstêp czasowy pomiêdzy klientami
+		min_per_cust = MIN_PER_HR / perhour;
+
+		// rozpoczêcie symulacji
+		for (int cycle = 0; cycle < cyclelimit; cycle++)
 		{
-			if (line.isfull())
-				turnaways++;
-			else
+			if (newcustomer(min_per_cust))  // mamy nowego chêtnego
 			{
-				customers++;
-				temp.set(cycle);        // czas przybycia = nr cyklu
-				line.enqueue(temp);     // do³¹czenie klienta do kolejki
+				if (line.isfull())
+					turnaways++;
+				else
+				{
+					customers++;
+					temp.set(cycle);        // czas przybycia = nr cyklu
+					line.enqueue(temp);     // do³¹czenie klienta do kolejki
+				}
 			}
+			if (wait_time <= 0 && !line.isempty())
+			{
+				line.dequeue(temp);        // nastêpny do obs³u¿enia
+				wait_time = temp.ptime();   // czas obs³ugi = wait_time
+				line_wait += cycle - temp.when();
+				served++;
+			}
+			if (wait_time > 0)
+				wait_time--;
+			sum_line += line.queuecount();
 		}
-		if (wait_time <= 0 && !line.isempty())
+
+		// zestawienie wyników
+		if (customers > 0)
 		{
-			line.dequeue(temp);        // nastêpny do obs³u¿enia
-			wait_time = temp.ptime();   // czas obs³ugi = wait_time
-			line_wait += cycle - temp.when();
-			served++;
+			cout << "\nCustomers:\n";
+			cout << "Customers Accepted:\t" << customers << endl;
+			cout << "Customers Served:\t" << served << endl;
+			cout << "Customers Turned away:\t" << turnaways << endl;
+			cout << "Average Queue length:\t";
+			cout.precision(2);
+			cout.setf(ios_base::fixed, ios_base::floatfield);
+			cout.setf(ios_base::showpoint);
+			cout << (double)sum_line / cyclelimit << endl;
+			cout << "Average wait time: "
+				<< (double)line_wait / served << " minute/s\n";
 		}
-		if (wait_time > 0)
-			wait_time--;
-		sum_line += line.queuecount();
-	}
-
-	// zestawienie wyników
-	if (customers > 0)
-	{
-		cout << " liczba klientów przyjêtych: " << customers << endl;
-		cout << "liczba klientów obs³u¿onych: " << served << endl;
-		cout << " liczba klientów odes³anych: " << turnaways << endl;
-		cout << "    œrednia d³ugoœæ kolejki: ";
-		cout.precision(2);
-		cout.setf(ios_base::fixed, ios_base::floatfield);
-		cout.setf(ios_base::showpoint);
-		cout << (double)sum_line / cyclelimit << endl;
-		cout << "    œredni czas oczekiwania: "
-			<< (double)line_wait / served << " minut\n";
-	}
-	else
-		cout << "Brak klientów!\n";
-
-	cout << "Gotowe!\n";
-
+		else
+			cout << "No clients!\n";
+	} while (((double)line_wait / served) >= 1.0);
+	cout << "ready!\n";
+	_getch();
 	return 0;
 }
 
